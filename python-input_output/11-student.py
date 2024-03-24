8cd7a51953fb3e67d5cb11ec587027a295a1417b
Ange-Constance <constance.nimuhire@gmail.com>
2024-03-24 13:41:40 -0700

    input output

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python3
-import os
-import sys
-
-Student = __import__('11-student').Student
-read_file = __import__('0-read_file').read_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-path = sys.argv[1]
-
-if os.path.exists(path):
-    os.remove(path)
-
-student_1 = Student("John", "Doe", 23)
-j_student_1 = student_1.to_json()
-print("Initial student:")
-print(student_1)
-print(type(student_1))
-print(type(j_student_1))
-print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+"""class_to_json"""
 
 
-save_to_json_file(j_student_1, path)
-read_file(path)
-print("\nSaved to disk")
+class Student:
+    """Contains student data
+    """
 
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-print("Fake student:")
-new_student_1 = Student("Fake", "Fake", 89)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    def to_json(self, attrs=None):
+        """class_to_json"""
 
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
 
-print("Load dictionary from file:")
-new_j_student_1 = load_from_json_file(path)
+    def reload_from_json(self, json):
+        """reload_from_json"""
 
-new_student_1.reload_from_json(j_student_1)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age)):
+        for items in json.keys():
+            self.__dict__[items] = json[items]
